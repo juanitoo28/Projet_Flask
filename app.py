3fe3896b473ea97f79ca6f1a7300b6ec3a0302cd
juanito mac <jean28tiago@gmail.com>
2023-05-12 15:19:42 +0200

    Migration du fichier Django vers Flask

--- app.py
+++ app.py
@@ -1,10 +1,77 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify, make_response
+from werkzeug.utils import secure_filename
+from openpyxl import load_workbook
+import os
+import io
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'  # chemin d'accès pour les fichiers uploadés
+
+# Vérifie si le dossier d'upload existe, sinon le crée
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'excel_file' in request.files:
+            file = request.files['excel_file']
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            response = make_response(redirect('/'))
+            response.set_cookie('file_path', file_path)
+            return response
+        else:
+            return 'No file part', 400
+    else:
+        return 'Method Not Allowed', 405
+
+@app.route('/download/')
+def download():
+    file_path = request.cookies.get('file_path')
+    if file_path and os.path.exists(file_path):
+        return send_file(file_path, as_attachment=True)
+    else:
+        return 'File Not Found', 404
+
+def add_sheet(file_path):
+    wb = load_workbook(filename=file_path)
+    ws = wb.create_sheet("Nouvelle feuille")
+    ws.cell(row=1, column=1, value="Exemple de contenu")
+    wb.save(file_path)
+
+@app.route('/add_sheet/', methods=['POST'])
+def add_sheet_view():
+    file_path = request.cookies.get('file_path')
+    if file_path and os.path.exists(file_path):
+        add_sheet(file_path)
+        return 'Sheet Added', 200
+    else:
+        return 'File Not Found', 404
+
+@app.route('/excel_preview/', methods=['POST'])
+def excel_preview():
+    if 'excel_file' in request.files:
+        file = request.files['excel_file']
+        wb = load_workbook(io.BytesIO(file.read()))
+        ws = wb.active
+
+        data = []
+
+        for row in ws.iter_rows(values_only=True):
+            data.append(list(row))
+
+        return jsonify({"data": data})
+    else:
+        return jsonify({"error": "Invalid request method"})
+    
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,123 @@
-console.log("Hello from JavaScript!");
+/* 
+Ce fichier JavaScript fait partie de l'application 'mon_application' du projet Django 'mon_projet'. 
+
+Il définit plusieurs fonctions qui sont utilisées pour interagir avec l'interface utilisateur, telles que :
+1. L'ajout d'une nouvelle feuille à un fichier Excel lorsqu'un bouton est cliqué ('runFunction' event listener).
+2. La navigation fluide entre les sections de la page grâce à l'écouteur d'événements de navigation.
+3. La lecture d'un fichier Excel côté client et l'affichage de son aperçu.
+4. La lecture d'un fichier Excel côté serveur et l'affichage de son aperçu.
+5. L'affichage de l'aperçu d'un fichier Excel dans un tableau HTML.
+6. L'écouteur d'événements qui déclenche la lecture du fichier Excel et son aperçu lorsque l'utilisateur sélectionne un fichier.
+
+Pour plus d'informations sur les concepts utilisés dans ce fichier, tels que les Fetch API, les promesses, async/await et les événements, consultez :
+- Fetch API : https://developer.mozilla.org/fr/docs/Web/API/Fetch_API
+- Promesses et async/await : https://developer.mozilla.org/fr/docs/Learn/JavaScript/Asynchronous
+- Événements : https://developer.mozilla.org/fr/docs/Web/API/Event
+- FileReader API : https://developer.mozilla.org/fr/docs/Web/API/FileReader
+*/
+
+document.getElementById("runFunction").addEventListener("click", async function () {
+    try {
+        const response = await fetch("/add_sheet/", {
+            method: "POST",
+        });
+
+        if (response.status === 200) {
+            alert("Feuille ajoutée avec succès !");
+        } else {
+            alert("Erreur lors de l'ajout de la feuille.");
+        }
+    } catch (error) {
+        console.error(error);
+        alert("Erreur lors de l'ajout de la feuille.");
+    }
+});
+
+
+document.querySelectorAll("nav a").forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const targetId = event.target.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
+
+function readExcelFile(file) {
+    const fileReader = new FileReader();
+    fileReader.onload = (event) => {
+        const data = new Uint8Array(event.target.result);
+        const workbook = XLSX.read(data, { type: "array" });
+
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const sheetData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+        displayExcelPreview(sheetData, "côté client");
+    };
+    fileReader.readAsArrayBuffer(file);
+}
+
+async function readExcelFileServerSide(file) {
+    const formData = new FormData();
+    formData.append("excel_file", file);
+
+    try {
+        const response = await fetch("/excel_preview/", {
+            method: "POST",
+            body: formData
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            displayExcelPreview(data.data, "côté serveur");
+        } else {
+            alert("Erreur lors de l'aperçu du fichier.");
+        }
+    } catch (error) {
+        console.error(error);
+        alert("Erreur lors de l'aperçu du fichier.");
+    }
+}
+
+
+function displayExcelPreview(sheetData, source) {
+    const previewContainer = document.getElementById("file_preview");
+    const table = document.createElement("table");
+    const sourceText = document.createElement("h3");
+    sourceText.textContent = `Aperçu du fichier Excel (${source})`;
+
+    sheetData.forEach((rowData) => {
+        const row = document.createElement("tr");
+
+        rowData.forEach((cellData) => {
+            const cell = document.createElement("td");
+            cell.textContent = cellData;
+            row.appendChild(cell);
+        });
+
+        table.appendChild(row);
+    });
+
+    previewContainer.innerHTML = "";
+    previewContainer.appendChild(sourceText);
+    previewContainer.appendChild(table);
+}
+
+
+document.getElementById("excel_file").addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const previewMethod = document.querySelector('input[name="preview_method"]:checked').value;
+
+    if (file) {
+        if (previewMethod === "client_side") {
+            readExcelFile(file);
+        } else {
+            readExcelFileServerSide(file);
+        }
+    } else {
+        document.getElementById("file_preview").innerHTML = "";
+    }
+});
